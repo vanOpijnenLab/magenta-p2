4212149a2ef2388a9a4ea5320f8f830cc76bcd3a
Margaret Antonio <antoniom@bc.edu>
2015-10-06 20:46:24 -0400

    Editing file to export tabular data for R Gviz annotation track

--- getCoordsGBK.py
+++ getCoordsGBK.py
@@ -1,7 +1,8 @@
 from Bio import SeqIO
+import sys
 
-gbk_filename = "NC_003028.gbk"
-faa_filename = "tigr4_convereted.faa"
+gbk_filename = sys.argv[2]
+faa_filename = sys.argv[3]
 
 output_handle = open(faa_filename, "w")
 
@@ -10,11 +11,14 @@ for seq_record in SeqIO.parse(gbk_filename, "genbank") :
     for seq_feature in seq_record.features :
         if seq_feature.type=="CDS":
             assert len(seq_feature.qualifiers['translation'])==1
-            output_handle.write(">%s from %s\n%s\n" % (
-                                                       seq_feature.qualifiers['locus_tag'][0],
-                                                       seq_record.name,
-                                                       seq_feature.qualifiers['translation'][0]))
-            print(int(seq_feature.location.start),int(seq_feature.location.end),seq_feature.strand)
+            width=seq_feature.location.end-seq_feature.location.start
+            output_handle.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (
+                    "NC_003028",
+                    seq_feature.location.start,
+                    seq_feature.location.end,
+                    width,
+                    seq_feature.strand,
+                    seq_feature.qualifiers['locus_tag'][0]))
 
 output_handle.close()
 print "Done"
