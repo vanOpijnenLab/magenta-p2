117cd6c5ff91280da3e15d54e596244936b24944
Margaret Antonio <antoniom@bc.edu>
2015-10-13 08:51:49 -0400

    Functional script--gets gene coordinates in GBK file for GRange format

--- getCoordsGBK.py
+++ getCoordsGBK.py
@@ -1,10 +1,17 @@
+#Margaret Antonio modified from
+
 from Bio import SeqIO
 import sys
 
-gbk_filename = sys.argv[2]
-faa_filename = sys.argv[3]
+gbk_filename = sys.argv[1]
+faa_filename = sys.argv[2]
+
+print (gbk_filename)
+print (faa_filename)
 
 output_handle = open(faa_filename, "w")
+output_handle.write("%s\t%s\t%s\t%s\t%s\t%s\n" % ("seqnames","start","end","width","strand","id"))
+
 
 for seq_record in SeqIO.parse(gbk_filename, "genbank") :
     print "Dealing with GenBank record %s" % seq_record.id
@@ -12,6 +19,7 @@ for seq_record in SeqIO.parse(gbk_filename, "genbank") :
         if seq_feature.type=="CDS":
             assert len(seq_feature.qualifiers['translation'])==1
             width=seq_feature.location.end-seq_feature.location.start
+
             output_handle.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (
                     "NC_003028",
                     seq_feature.location.start,
