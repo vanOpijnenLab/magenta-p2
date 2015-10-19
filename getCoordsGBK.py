0b8643ddc79a9ab98fb26fc632e0d1e11e464c63
Margaret Antonio <antoniom@bc.edu>
2015-10-18 21:25:52 -0400

    Script for getting gene names and coordinates from a gbk file and formatting for Gviz in R

--- getCoordsGBK.py
+++ getCoordsGBK.py
@@ -1,26 +1,29 @@
-#Margaret Antonio modified from
+#Margaret Antonio 15.10.18
+
+#Use this script to get gene names and coordinates from a GBK file
+#Output file can be inputted into Gviz as an annotation track for visualization
 
 from Bio import SeqIO
 import sys
 
-gbk_filename = sys.argv[1]
-faa_filename = sys.argv[2]
+gbkFile = sys.argv[1]
+customFile = sys.argv[2]
 
-print (gbk_filename)
-print (faa_filename)
+print (gbkFile)
+print (customFile)
 
-output_handle = open(faa_filename, "w")
-output_handle.write("%s\t%s\t%s\t%s\t%s\t%s\n" % ("seqnames","start","end","width","strand","id"))
+out = open(customFile, "w")
+out.write("%s\t%s\t%s\t%s\t%s\t%s\n" % ("seqnames","start","end","width","strand","id"))
 
 
-for seq_record in SeqIO.parse(gbk_filename, "genbank") :
-    print "Dealing with GenBank record %s" % seq_record.id
+for seq_record in SeqIO.parse(gbkFile, "genbank") :
+    print "Extracting gene coordinates from GBK file: %s" % seq_record.id
     for seq_feature in seq_record.features :
         if seq_feature.type=="CDS":
             assert len(seq_feature.qualifiers['translation'])==1
             width=seq_feature.location.end-seq_feature.location.start
 
-            output_handle.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (
+            out.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (
                     "NC_003028",
                     seq_feature.location.start,
                     seq_feature.location.end,
@@ -28,5 +31,5 @@ for seq_record in SeqIO.parse(gbk_filename, "genbank") :
                     seq_feature.strand,
                     seq_feature.qualifiers['locus_tag'][0]))
 
-output_handle.close()
+out.close()
 print "Done"
