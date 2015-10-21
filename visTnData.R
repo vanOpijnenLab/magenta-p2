99ea4ee64ced23c5bf7b3e3d11bdefdf456a9db6
Margaret Antonio <antoniom@bc.edu>
2015-10-20 21:52:00 -0400

    Gene track and indiv. fitness track disappears if window is too populated

--- visTnData.R
+++ visTnData.R
@@ -1,4 +1,4 @@
-getTracks<-function(fromCoord,toCoord){
+getTracks<-function(fromCoord,toCoord,x,y){
   #Load libraries
   library(seqinr)
   library(Biostrings)
@@ -8,7 +8,6 @@ getTracks<-function(fromCoord,toCoord){
   geneFile="tigr4_genes.txt"
   singleFitFile="singleFit.txt"
   
-
   #so Gviz doesn't look for NC_003028 in the UCSC site
   options(ucscChromosomeNames = FALSE)
   
@@ -31,41 +30,59 @@ getTracks<-function(fromCoord,toCoord){
   #singleFitFile="singleFit.txt"
   insertFit<-as.data.frame(read.table(file=singleFitFile))
   colnames(insertFit)<-c("seqnames","start","end","fit")
+  insertFit$fit=insertFit$fit-1;
   insertFit$seqnames="NC_003028"
   insertData<-makeGRangesFromDataFrame(insertFit,keep.extra.columns=TRUE,ignore.strand=TRUE,seqinfo=NULL,seqnames.field="seqnames",start.field="start",starts.in.df.are.0based=FALSE)
   aggTrack<-DataTrack(insertData,chromosome="NC_003028",ylim=c(-1,1),type="histogram",
                       col.baseline="black",baseline=0,fill="darkred",
                       name="Insertions")
   #background.panel="#FFFFE0",
-  #AnnotationTrack2: Fitness values that corresponding to DataTrack1
   
+  #AnnotationTrack2: Fitness values that corresponding to DataTrack1
+  output<-Wavg(fit,fromCoord,toCoord)
+  avg<-round(as.numeric(output[1]),digits=4)
+  count<-output[2]
+  avgText<-paste0("Average= ",avg)
+  avgTrack<-AnnotationTrack(start=fromCoord,end=toCoord,chromosome="NC_003028",id=avgText,showFeatureId=TRUE,fill="darkred")
+  
+  #Track for individual fitness values when the window is small enough
   fitTrack<-AnnotationTrack(insertFit,chromosome="NC_003028",
                             showFeatureId=TRUE,showId=FALSE,id=insertFit$fit,fill="transparent",
                             fontcolor.item="black",col.frame="transparent",
                             background.title="transparent",col="transparent",name="Fitness Value")
   
-  Wavg<-function(fit,fromCoord,toCoord){
-    sum=0; count=0;i=1;
-    while (fit[i,2]<=fromCoord) i<-i+1
-    while (fit[i,2]<toCoord){
-      sum<-sum+fit[i,4]; count<-count+1;
-      print(fit[i,4]);
-      i<-i+1;
-    }
-    avg<-(sum/count);
-    return(avg);
+#Keep track pollution low:
+  if (count>40 || (toCoord-fromCoord>10000)){
+    ht1 <- HighlightTrack(trackList = list(gTrack,sTrack,aggTrack,avgTrack), start =fromCoord,end= toCoord,chromosome = "NC_003028")
   }
-  
-  
-  
-  ht1 <- HighlightTrack(trackList = list(gTrack,sTrack,anTrack,aggTrack,fitTrack), start =fromCoord,end= toCoord,chromosome = "NC_003028")
+  else{
+    ht1 <- HighlightTrack(trackList = list(gTrack,sTrack,anTrack,aggTrack,fitTrack,avgTrack), start =fromCoord,end= toCoord,chromosome = "NC_003028")
+    
+  }
   #plotTracks(list(gTrack,sTrack,anTrack,aggTrack,fitTrack),
              #background.title="darkred",fontsize=17,chromosome="NC_003028",
              #from=fromCoord,to=toCoord)
-  plotTracks(ht1,chromosome="NC_003028",background.title="darkred",fontsize=17,from=fromCoord-15,to=toCoord+15)
   
-  confirm=paste0("Tracks plotted for genomic coordinates ",fromCoord-10," to ",toCoord+10,".")
+  #TO CHANGE SURROUNDING WINDOWS ALTER:
+  if (missing(x))x=50
+  if (missing(y))y=50
+
+  plotTracks(ht1,chromosome="NC_003028",background.title="darkred",fontsize=17,from=fromCoord-x,to=toCoord+y)
+  
+  confirm=paste0("Tracks plotted for genomic coordinates ",fromCoord," to ",toCoord," with a window (",fromCoord-x,",",toCoord+y,")")
   return(confirm)
   
   
+}
+
+Wavg<-function(fit,fromCoord,toCoord){
+  sum=0; count=0;i=1;
+  while (fit[i,2]<=fromCoord) i<-i+1
+  while (fit[i,2]<toCoord){
+    sum<-sum+fit[i,4]; count<-count+1;
+    i<-i+1;
+  }
+  avg<-(sum/count)-1;
+  output<-list(avg,count)
+  return(output);
 }
