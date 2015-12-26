d7981673f6b476d655d15a5dd9c5d5182e892375
Margaret Antonio <margaretantonio@gmail.com>
2015-12-26 13:32:11 -0500

    Filter to identify cold spots in essential regions

--- test.pl
+++ test.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/perl -w
 
-#Margaret Antonio 15.12.18
+#Margaret Antonio 15.12.26
+
+#DESCRIPTION: Filter to identify which "essential" regions are actually "cold spots", here defined as
+# regions with 2000 bp before and after the designated region that also have low insertion
+# rate (i.e. they are also defined as "essential" with significant p-values)
 
