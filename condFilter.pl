c0f448bbf16e280ca35dcc6598679d0e5d277771
Margaret Antonio <antoniom@bc.edu>
2016-01-13 00:33:45 -0500

    efficiency changes

--- condFilter.pl
+++ condFilter.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/perl -w
 
-#Margaret Antonio 15.12.26
+#Margaret Antonio Began: 15.12.26 Updated: 16.01.11
 
-#DESCRIPTION: Filter to identify which "essential" regions are actually "cold spots", 
-# here defined as regions with 2000 bp before and after the designated region that also
-# have low insertion rate (i.e. they are also defined as "essential" with significant 
-# p-values)
+#DESCRIPTION: Filter to remove windows (lines) that satisfy the specified condition
+# Where i=number, conditions include <i, >i <=i, >=i, ==i, !=i, etc.
+
+#allow multiple ri rc!!!!
 
 use strict;
 use Getopt::Long;
 use warnings;
 use Text::CSV;
-no warnings 'recursion';
+use diagnostics;
 
 #USAGE from /8-essFilters
 #perl ../Blueberries/test.pl --in slidingWindowF.csv
@@ -23,51 +23,50 @@ our ($in,$h,$out,$rIndex,$rCond);
 GetOptions(
 'i:s' => \$in,
 'o:s' =>\$out,
-'usage' => \$h,
-'r:i' => \$rIndex,
-'c:i' => \$rCond,
+'help' => \$h,
+'ri:i' => \$rIndex,  #removal index
+'rc:s' => \$rCond,   #removal condition
 );
 
+#if ($h){
+    # print "Help information coming soon..." and die "Goodbye..."; #must fix this!!!
+    # removal filter can be < > <= >= == or !=
+    # }
 
-if (!$out) {$out="removalFilt.csv"};
-if (!$rIndex) {$rIndex=2}; #Removal of index
-if (!$rCond) {$rCond=0};    #If entry at index meets this condition
+#SET DEFAULTS IF NOTHING SPECIFIED FOR REQUIRED OPTIONS
+if (!$out) {$out="filt.csv"};
+if (!$rIndex) {$rIndex=2};      #Removal of index
+if (!$rCond) {$rCond="==0"};    #If entry at index meets this condition
 
 #GET DATA OUT OF FILE AND INTO 2D ARRAY
 my @outArray;
 
 sub cleaner{
 	my $line=$_[0];
+    print "This is it ", $_[0];
 	chomp($line);
 	$line =~ s/\x0d{0,1}\x0a{0,1}\Z//s; 
 	return $line;
 	}
-	
-my $csv = Text::CSV->new({ binary => 1, auto_diag => 1, eol => "\n"}) or die "Cannot use CSV: " . Text::CSV->error_diag();
-open (my $outfile,'>',"$out")or die "Could not open '$out' \n";
+
+open (OUT,'>',"$out")or die "Could not open '$out' \n";
 
 open(DATA, '<', "$in") or die "Could not open '$in' \n";
 	
-my $line=<DATA>;
-#print "This is the line: ",$line,"stoooooop";
-$line=cleaner($line); #gets rid of carriage returns (^M)
-my @header=split(',',$line);
-push (@header,'csTest');
-my $tick=0;
-$csv->print($outfile, \@header); #header
+my $head=<DATA>;
+$head=cleaner($head); #gets rid of carriage returns (^M)
+my @header=split(',',$head);
+print OUT (join(",",@header),"\n"); #header
 
 while (my $entry = <DATA>) {
-	#$tick+=1;
-	#print $tick, "\t";
+    print $entry;
 	$entry=cleaner($entry);
-	#chomp($entry);
-	#$entry =~ s/\x0d{0,1}\x0a{0,1}\Z//s; 
 	my @fields=split(',',$entry);
-    if ($fields[$rIndex]!=$rCond){
-        #push (@outArray,\@fields); #should be ref array
-        $csv->print($outfile, \@fields);
+    #if ($fields[$rIndex]!=$rCond){
+    if (!eval($fields[$rIndex].$rCond)){
+        print OUT (join(",",@fields),"\n");
     }
 }
-
 close DATA;
-close $outfile;
+close OUT;
+
