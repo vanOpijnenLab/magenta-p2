bc089b31e295483258e2249dcb15ded31e857179
Margaret Antonio <antoniom@bc.edu>
2016-01-15 16:49:55 -0500

    Added comments and standard options

--- compGenes.pl
+++ compGenes.pl
@@ -2,12 +2,7 @@
 
 #Margaret Antonio 16.01.13
 
-#DESCRIPTION: Takes two aggregate.pl outputs and compares them using mean difference, pval for each
-#gene. Can compare, for example, control vs antibiotic.
-
-#Improvements to be made: default output should b
-
-#USAGE: perl compGenes.pl <aggregateFile1.csv aggregateFile2.csv> OR -indir <indir/>
+#USAGE: perl compGenes.pl <options> <aggregateFile1.csv aggregateFile2.csv> OR -indir <indir/>
 
 use Data::Dumper;
 use strict;
@@ -18,25 +13,46 @@ use File::Path;
 use File::Basename;
 use Statistics::Distributions;
 
-#ASSIGN INPUTS TO VARIABLES
+#ASSIGN INPUTS TO VARIABLES USING FLAGS
 our ($indir,$h,$out,$sortkey,$round,$l);
 GetOptions(
-'indir:s' => \$indir,
+'d:s' => \$indir,
+'h' => \$h,
 'o:s' =>\$out,
 's:i' => \$sortkey,
 'r:i'=> \$round,
 'l:s'=> \$l,
 );
 
-sub get_time() {
-    my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
-    return "$hour:$min:$sec";
+sub print_usage() {
+    print "\n";
+    print "compGenes.pl: COMPARE GENES FROM A TN-SEQ EXPERIMENT USING AGGREGATE FILES\n\n";
+    print "DESCRIPTION: Takes two aggregate.pl outputs and compares them by calculating\n";
+    print "the difference in mean fitness, the pval for each gene.\n";
+    print "Example: compare control vs antibiotic, where both from same strain (genome).\n";
+    print "For different strains/genomes, use compStrains.pl\n";
+    print "\nUSAGE: perl compGenes.pl <options> <aggregateFile1.csv aggregateFile2.csv>\n";
+    print "       or -d <directory/> \n\n";
+    print "OPTIONS:\n\n";
+    print " -h\tPrints usage and quits\n";
+    print " -d\tDirectory containing input files. Make sure / is included after name\n";
+    print " -o\tOutput file for comparison data. Default: compFile1File2.csv\n";
+    print " -s\tSort output by this index of the file (indices begin at 0). Default: by mean\n";
+    print " -r\tRound final output numbers to this number of decimals\n";
+    print " -l\tLabels for for compared files. Used for column names and default output file name.\n";
+    print "   \tTwo strings, comma separated (i.e. -l gluc,dapto). Order should match file order.\n";
+    print "\n\n";
+}
+if ($h){
+    print_usage();
+    exit;
 }
 
+#THE @files ARRAY WILL CONTAIN INPUT FILE NAMES, EXTRACTED FROM A DIRECTORY (-indir) OR ARGV
 my @files;
 if ($indir){
     my $directory="$indir";
-    opendir(DIR, $directory) or die "couldn't open $directory: $!\n";
+    opendir(DIR, $directory) or die "Couldn't open $directory: $!\n";
     my @direct= readdir DIR;
     my $tail=".csv";
     foreach (@direct){
@@ -50,17 +66,14 @@ if ($indir){
 else{
     @files=@ARGV;
 }
-my $num=(scalar @files);
 
+#GET LABELS: USE (-l) OR USE FILNEAMES AS LABELS FOR COLUMNS IN OUTPUT FILE
 
-my @header;
-push (@header,"id");
 my @labels;
 if ($l){
     @labels=split(',',$l);
 }
 else{
-    #get rid of .csv part in file names and use as labels
     foreach (@files){
        my @temp=split('\\.',$_);
        my $colName=$temp[0];
@@ -68,31 +81,33 @@ else{
    }
 }
 
+#CHECK IF REQ. VARIABLES WERE DEFINED USING FLAGS. IF NOT THEN USE DEFAULT VALUES
+
 if (!$out) {$out="comp-".$labels[0].$labels[1].".csv"}
 if (!$round){$round='%.4f'}
 
-
-
-#push (@header,@sample);
+#READ INPUT FILES AND PUT GENE INFO FROM EACH LINE INTO HASH %all WHERE KEY=GENE_ID AND
+#VALUE=GENE INFO (MEAN FITNESS, # INSERTIONS, ETC.)
 
 my %all;
-for (my $i=0; $i<$num; $i++){   #Read files from ARGV
+my @header;
+for (my $i=0; $i<2; $i++){
     print "File #",$i+1,"\t";
-    
     my $file=$files[$i];
     print $file,"\n";
     
-    open(DATA, '<', $file) or die "Could not open '$file' Make sure input .csv files are entered in the command line\n";
+    open(DATA, '<', $file) or die "Could not open '$file'\n";
     
-    #extract the header
-    my $head=<DATA>; #the header in the file
+    #EXTRACT THE HEADER (COLUMN NAMES) OF THE FILE AND KEEP FOR OUTPUT HEADER
+    #APPEND FILE NAME OR GIVEN LABEL (-l) TO COLUMN NAME SO ITS DIFFERENT FROM OTHER INPUT FILE
+    
+    my $head=<DATA>;
     my @cols=split(',',$head);
-     @cols=@cols[1,2,3,4,5,6]; #get rid of gene name
+    @cols=@cols[0,1,2,3,4,5,6];
     for (my $j=0;$j<scalar @cols;$j++){
         $cols[$j]=$cols[$j].'-'.$labels[$i];
     }
     push (@header,@cols);
-    #my %hash;
     while (my $entry = <DATA>) {
         chomp $entry;
         my @line=split(",",$entry);
@@ -105,10 +120,9 @@ for (my $i=0; $i<$num; $i++){   #Read files from ARGV
         @line=@line[0,1,2,3,4,5,6];
         my $gene=$line[0];
         chomp($gene);
-        #my $mean = sprintf("$round",$line[1]);
-        shift @line; #pop off the "gene name"
         
-        ############################## hash assignment
+        #PUT GENE AND INFO INTO THE HASH FOR EXISTING KEY (1ST FILE) OR CREATE NEW KEY (2ND FILE)
+        
         if(!exists $all{$gene}){
             my @info;
             push (@info,@line);
@@ -117,72 +131,61 @@ for (my $i=0; $i<$num; $i++){   #Read files from ARGV
         else{
             my @info=@{$all{$gene}};
             push (@info,@line);
-            my $diff=sprintf("$round",($info[0]-$info[6]));
-            my $total1=$info[5];
-            my $total2=$info[11];
-            my $sd1=$info[2];
-            my $se1=$info[3];
-            my $sd2=$info[8];
-            my $se2=$info[9];
+            my $diff=sprintf("$round",($info[1]-$info[8]));
+            my $total1=$info[6];
+            my $total2=$info[13];
+            my $sd1=$info[3];
+            my $se1=$info[4];
+            my $sd2=$info[10];
+            my $se2=$info[11];
             my $df=$total1+$total2-2;
             my $tdist;
             my $pval;
-            #print $gene, "\t",$total1,"\t",$total2,"\n";
+            
+            # CHECK TO MAKE SURE ALL VARIABLES IN TDIST,PVAL CALCULATIONS ARE NUMBERS AND NO
+            # ZEROS (0) IN THE DENOMINATOR
+            
             if ($se1 eq "X" or $se2 eq "X" or $sd1 eq "X" or $sd2 eq "X" or $total1==0 or $total2==0 or $sd1==0 or $sd2==0 or $df<=0){
                 ($tdist,$pval)=("NA","NA");
             }
             else{
-    
                 $tdist=sqrt((($diff)/(sqrt((($sd1**2)/$total1)+(($sd2**2)/$total2))))**2);
                 $pval=Statistics::Distributions::tprob($df,$tdist);
             }
             push (@info,$diff,$df,$tdist,$pval);
             $all{$gene}=\@info;
-    
-          
         }
-       
     }
-    
     close DATA;
 }
 
-
-#PRINT TO OUT FILE
-
-#Set up the header (column names)
-
+#READ ALL HASH CONTENTS INTO 2D ARRAY FOR EASY SORTING AND PRINTING TO OUT FILE
 my @unsorted;
 
 foreach my $entry (keys %all) {
     my @info=@{$all{$entry}};
-    #print $entry,",";      #if unhashed, must go after OPEN
-    #print (join(",",@info),"\n");
     my @temp;
-    push (@temp,$entry);
     push (@temp,@info);
     push (@unsorted,\@temp);
 }
 
+#SORT GENES BY PVALUE OR FITNESS DEPENDING ON WHICH FLAG WAS USED IN COMMANDLINE
 if (!$sortkey){
-    $sortkey=13; #for mean difference
+    $sortkey=14; #default: sort by difference of means
 }
 my @sorted = sort { $b->[$sortkey] <=> $a->[$sortkey] } @unsorted;
 
-#Finish up the header
+#ADD NEW FIELDS TO HEADER (COLUMN NAMES)
 my $field="Mean".$labels[0].'-'.$labels[1];
 push (@header,$field,"DOF","TDIST","PVALUE");
 
+#PRINT TO OUT FILE
 open OUT, '>',"$out";
 print OUT (join(',',@header),"\n");
 foreach (@sorted){
     my @woo=@{$_};
-    #print scalar @woo,"\t";
     print OUT join(',',@woo),"\n";
     }
-
-    #print OUT (join(',',@foo),"\n");
-
 close OUT;
 
 
