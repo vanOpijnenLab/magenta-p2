e226eb94ca9c2cb4383bd93b3e2cff99d38a8f57
Margaret Antonio <margaretantonio@gmail.com>
2016-08-30 19:48:53 -0400

    Updated usage and flags

--- tools/condFilter.pl
+++ tools/condFilter.pl
@@ -12,10 +12,10 @@ use Text::CSV;
 use diagnostics;
 
 #ASSIGN INPUTS TO VARIABLES
-our ($h,$out,$kIndex,$kOper,$kVal);
+our ($help,$out,$kIndex,$kOper,$kVal);
 GetOptions(
 'o:s' =>\$out,
-'h' => \$h,
+'h' => \$help,
 'ki:i' => \$kIndex,   #keep index #any number that is an index (i for integer)
 'ko:s' => \$kOper,    #keep operator condition. Can be e,l,g,le,ge,ne. (s for string)
 'kv:f' => \$kVal,     #keep value #any number (f for float)
@@ -24,23 +24,30 @@ GetOptions(
 
 sub print_usage() {
     print "\n";
-    print "condFilter.pl: Conditionally filter out lines in a csv file\n\n";
+    print "\n####################################################################\n";
+    print "\n";
+    print "condFilter.pl: Conditionally filter lines in a csv file\n\n";
     print "DESCRIPTION: Takes a single file and keeps only lines that satisfy condition\n";
     print "the difference in mean fitness, the pval for each gene.\n";
-    print "Example: Keep all lines in a comparison file where the difference between\n";
+    print "EXAMPLE: Keep all lines in a comparison file where the difference between\n";
     print "\tmean fitnesses is greater than .15\n";
-    print "\nUSAGE: perl condFilter.pl -ki <index of col> -ko <e,l,g,le,ge,or ne>\n";
-    print "\t-kv <keep value> <options> <file.csv>\n\n";
+    
+    print "\nUSAGE: perl condFilter.pl -ki 5 -ko .05 -kv le inputfile.csv\n\n";
+    
     print "OPTIONS:\n\n";
-    print "-ki\t Index of column to test keep value and keep operator. Indices start at 0.\n";
-    print "-ko\t String version of operator to use (, l, g, le, ge, ne).\n";
-    print "-kv\t Value to use in conditional test.\n";
-    print " -h\t Prints usage and quits\n";
-    print " -o\t Output file for filtered data. Default: filt.csv\n";
-    print "\n\n";
+    print " -h \t Prints usage and quits\n";
+    print "    \t Input comma-separated-value file (CSV) without a flag\n";
+    print " -ki\t Index of column to test keep value and keep operator.\n";
+    print "    \t Indices begin at 0.\n";
+    print " -ko\t String version of operator to use (e, l, g, le, ge, ne).\n";
+    print " -kv\t Value to use in conditional test.\n";
+    print " -o \t Output file for filtered data. Default: filt.csv\n";
+
+    print "\n####################################################################\n";
+    print "\n";
 }
 #IF HELP FLAG (-h) WAS SPECIFIED THEN PRINT USAGE AND QUIT
-if ($h){
+if ($help){
     print_usage();
     exit;
 }
@@ -48,26 +55,27 @@ if ($h){
 #INTERPRET KEEP CONDITION FROM STRING TO OPERATOR. AND CHECK FOR OTHER PARTS OF CONDTIONAL TEST.
 #Can't directly use operator because > is interpreted as output into (> file)
 
-if ($kOper eq "e"){
+
+if ($kOper and $kOper eq "e"){
     $kOper="==";
 }
-elsif ($kOper eq "l"){
+elsif ($kOper and $kOper eq "l"){
     $kOper="<";
 }
-elsif ($kOper eq "g"){
+elsif ($kOper and $kOper eq "g"){
     $kOper=">";
 }
-elsif ($kOper eq "le"){
+elsif ($kOper and $kOper eq "le"){
     $kOper="<=";
 }
-elsif ($kOper eq "ge"){
+elsif ($kOper and $kOper eq "ge"){
     $kOper=">=";
 }
-elsif ($kOper eq "ne"){
+elsif ($kOper and $kOper eq "ne"){
     $kOper="!=";
 }
 else {
-    print "\nPlease enter one of the following for the keep operator (-ko): e, l, g, le, ge, ne\n\n";
+    print "\nPlease enter one of the following for the keep operator (-ko): e, l, g, le, ge, ne";
     print_usage();
     exit;
 }
@@ -94,8 +102,8 @@ sub cleaner{
 my @outArray;
 
 my $in=$ARGV[0];
-open (OUT,'>',"$out")or (print_usage() and die "Could not open '$out' \n");
-open(DATA, '<', "$in") or (print_usage() and die "Could not open '$out' \n");
+open (OUT,'>',"$out") or (print "Could not open '$out'\n" and print_usage() and exit);
+open(DATA, '<', "$in") or (print "Could not open '$in'\n" and print_usage() and exit);
 
 #REUSE the header (column names) from the input file for the output file
 my $head=<DATA>;
